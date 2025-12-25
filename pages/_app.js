316aa56edafb25b7bda1801b0c0ae918c7ee8844
Dário Hilário Baptista <hilariodario300@gmail.com>
2025-12-25 18:14:36 +0200

    Import PageChange component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
-
+ 
 import PageChange from "components/PageChange/PageChange.js";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
