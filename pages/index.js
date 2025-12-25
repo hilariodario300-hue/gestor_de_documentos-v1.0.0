dfa7f42eb263f9b7b41b17ce35e77826d35a34e2
Dário Hilário Baptista <hilariodario300@gmail.com>
2025-12-25 18:25:35 +0200

    Replace CardStats and CardRecentDocuments with charts

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,33 @@
 import React from "react";
 
 // components
-import CardStats from "components/Cards/CardStats.js";
-import CardRecentDocuments from "components/Cards/CardRecentDocuments.js";
-import CardSystemInfo from "components/Cards/CardSystemInfo.js";
 
-// layout
+import CardLineChart from "components/Cards/CardLineChart.js";
+import CardBarChart from "components/Cards/CardBarChart.js";
+import CardPageVisits from "components/Cards/CardPageVisits.js";
+import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+
+// layout for page
+
 import Admin from "layouts/Admin.js";
 
 export default function Dashboard() {
   return (
     <>
-      {/* Cards de estatísticas */}
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-3/12 px-4">
-          <CardStats
-            statTitle="Documentos"
-            statValue="1,248"
-            statIconName="fas fa-folder-open"
-            statIconColor="bg-lightBlue-500"
-          />
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardLineChart />
         </div>
-
-        <div className="w-full lg:w-3/12 px-4">
-          <CardStats
-            statTitle="Alunos"
-            statValue="842"
-            statIconName="fas fa-user-graduate"
-            statIconColor="bg-emerald-500"
-          />
-        </div>
-
-        <div className="w-full lg:w-3/12 px-4">
-          <CardStats
-            statTitle="Pendentes"
-            statValue="36"
-            statIconName="fas fa-clock"
-            statIconColor="bg-orange-500"
-          />
-        </div>
-
-        <div className="w-full lg:w-3/12 px-4">
-          <CardStats
-            statTitle="Usuários"
-            statValue="12"
-            statIconName="fas fa-users"
-            statIconColor="bg-purple-500"
-          />
+        <div className="w-full xl:w-4/12 px-4">
+          <CardBarChart />
         </div>
       </div>
-
-      {/* Conteúdo principal */}
       <div className="flex flex-wrap mt-4">
-        <div className="w-full lg:w-8/12 px-4">
-          <CardRecentDocuments />
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardPageVisits />
         </div>
-
-        <div className="w-full lg:w-4/12 px-4">
-          <CardSystemInfo />
+        <div className="w-full xl:w-4/12 px-4">
+          <CardSocialTraffic />
         </div>
       </div>
     </>
