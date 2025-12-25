12fe1320b35d45b867ec18de8c38e6dee6f94e34
Dário Hilário Baptista <hilariodario300@gmail.com>
2025-12-25 17:51:46 +0200

    Update app metadata and comments for Kalandula Docs

--- pages/_app.js
+++ pages/_app.js
@@ -31,20 +31,13 @@ export default class MyApp extends App {
     let comment = document.createComment(`
 
 =========================================================
-* Notus NextJS - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+* Kalandula Docs - v1.0.0 based on Tailwind 
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/notus-nextjs
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md)
 
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
 
 =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 `);
     document.insertBefore(comment, document.documentElement);
@@ -70,8 +63,8 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>Notus NextJS by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+          <title>Kalandula Docs</title>
+          <script src="https://www.ispeka.ao/index.php"></script>
         </Head>
         <Layout>
           <Component {...pageProps} />
